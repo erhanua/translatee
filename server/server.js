969d45af272011cc0e19b3d6d710322f88f9e379
Adınız Soyadınız <email@adresiniz.com>
2023-11-02 10:26:46 +0000

    tranlate

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(cors());
+const axios = require("axios");
+
+// add your endpoints here
+app;
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
